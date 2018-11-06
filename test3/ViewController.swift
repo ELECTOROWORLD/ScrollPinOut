b6b71c66d5e6da92c75b0352a71d383957d66d23
AokiRikuRiku <rikugithub@yahoo.co.jp>
2018-11-06 16:54:47 +0900

    Initial Commit

---
+++ test3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test3
+//
+//  Created by 青木　陸 on 2018/11/06.
+//  Copyright © 2018 Aoki Riku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
